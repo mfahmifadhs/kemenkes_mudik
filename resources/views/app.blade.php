dd63966c23b8520c554669f52036907f20af0b8b
mfahmifadhs <mfahmifadh2@gmail.com>
2025-02-24 12:05:39 +0700

    Update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -80,7 +80,7 @@
     </main>
 
     <!-- JAVASCRIPT FILES -->
-    <script src="{{ asset('dist/js/jquery.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="{{ asset('dist/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('dist/js/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('dist/js/custom.js') }}"></script>
